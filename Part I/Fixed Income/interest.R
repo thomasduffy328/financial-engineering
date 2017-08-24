333085ef1e0a5d9f7b73cac9f85d5081c6398c39
Thomas Duffy <tpd2115@columbia.edu>
2017-08-24 09:03:34 -0400

    Update interest.R

--- Part I/Fixed Income/interest.R
+++ Part I/Fixed Income/interest.R
@@ -15,70 +15,42 @@ pvFactor <- function(n, r) {
   return((1 + r)^-n)
 }
 
-LumpSum <- function(C, n, r) {
-  # calculate the present value, pv, of an lump sum C
-  # paid over n years with interest rate, r
-  return(C * pvFactor(n, r))
-}
-
-netPresentValue <- function(flows, n, r) {
-  # calculate the net present value, npv, of a stream
-  # of cash flows, flows, for n periods
-  # assumes fixed interest rate, r
-  if(mode(flows) == "numeric") {
-    # here we're accounting for fixed cash flow
-    flows <- rep(flows, n)
-  }
-  if(n != length(flows)) {
-    stop("There must be as many cash flows as periods")
+# work the kinks out here a little more
+DiscountRate(n, r) {
+  
+  if(length(r) == 1) {
+    return((1 + r)^-n)
   } else {
-    values <- vector("numeric", n)
-    for(i in 0:(n-1)) {
-      values[i + 1] <- flows[i + 1] * pvFactor(i, r)
-    }
-    npv <- sum(values)
-  }
-  return(npv)
-}
-
-PresentValue <- function(C0, rb, rl, N, borrow = T) {
-  # the present valu, pv, of the contract that pays c.sub cash
-  # flows for N periods
-  # assuming fixed interest rates, rl (lending) & rb (borrowing)
-  # that are not necessarily equal
-  pv <- C0
-  c.sub <- vector("numeric", N + 1)
-  c.sub[1] = C0
-
-  # price of borrowed contract 
-  # price = p - c0 - sum(k=1 to N) ck/(1+rb)^k
-  
-  # price of sold contract
-  # price = -p + c0 + sum(k=1 to N) ck/(1+rl)^k
-  
-  if(rl == rb) {
-    if(borrow == T) {
-      for(i in 2:N) {
-        pv = pv + c.sub[i]/((1 + rl)^i) 
-      }
-    } else {
-      
-    }
     
-  } else {
-    if(borrow == T) {
-      
-    } else {
-      
-    }
   }
-  return(pv)
-  
-  # NOTE: bounds on the price
-  # PV(c; rb) <= p <= PV(c;rl)
-  # again, rl <= rb
-  # this provides us with a price interval
-  # then supply & demand determine the price within that interval
 }
 
-# FIXED INCOME SECURITIES - 13:51
+PresentValue <- function(flows, rb, rl, N) {
+  # determines the present value of a portfolio of cash flows (flows)
+  # over N periods with a borrowing interest rate, rb, and a 
+  # a lending interest rate, rl
+  if(length(flows) == 1) {
+    flows <- rep(flows, N)
+  } else if(length(flows) != N) {
+    stop("There must be the same number of cash flows as periods")
+  }
+  if(rl == rb) {
+    pv <- flows[1]
+    for(i in 2:N) {
+      pv <- pv + flows[i] * pvFactor(i-1, rb)
+    }
+  output <- list("Value" = pv)
+  return(output)
+  } else {
+    pv.borrow <- flows[1]
+    pv.lend   <- flows[1]
+    for(i in 2:N-1) {
+      pv.borrow <- pv.borrow + flows[i] * pvFactor(i-1,rb)
+      pv.lend   <- pv.lend + flows[i] * pvFactor(i-1, rl)
+    }
+  output <- list("Borrow Value" = pv.borrow, "Lend Value" = pv.lend)
+  return(output)
+  }
+}
+
+
